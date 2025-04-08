9c9656b4da5d9a7ee8672d596016088f2275c5db
alibekashirali <ashiralialibek@gmail.com>
2025-04-08 11:43:04 +0000

    asdasda

--- _printf.c
+++ _printf.c
@@ -1,15 +1,6 @@
 #include <stdarg.h>
 #include <unistd.h>
-
-/**
- * _putchar - writes a character to stdout
- * @c: The character to print
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
+#include "main.h"
 
 /**
  * _printf - produces output according to a format
